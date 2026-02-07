6aee8bf0b471fd092e2594b6748c345f39f33977
lamortia <roboito_01@hotmail.com>
2026-02-07 00:01:01 -0500

    Update cache name from v12 to v13

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ft-cache-v12";
+const CACHE_NAME = "ft-cache-v13";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json"];
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache)=>cache.addAll(ASSETS)));
