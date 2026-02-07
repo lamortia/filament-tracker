14b81984ffe303cda150d948477ec0b9d02cb65a
lamortia <roboito_01@hotmail.com>
2026-02-06 23:32:46 -0500

    Update cache name from v10 to v11

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ft-cache-v10";
+const CACHE_NAME = "ft-cache-v11";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json"];
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache)=>cache.addAll(ASSETS)));
